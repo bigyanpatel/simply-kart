a633e327102aa9662c24b6afda80353b45adb13d
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-12-10 10:14:39 +0530

    Feat: Setup router at root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import {
+  Login,
+  Signup,
+  Products,
+  Wishlist,
+  Cart,
+  Home,
+} from "./pages/Pageutil";
 
 function App() {
-  
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
