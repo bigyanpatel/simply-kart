47058cbdc90357c4628f5267ae902ac069fea534
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-12-11 14:49:06 +0530

    Style: Changed styles

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -9,31 +9,35 @@ export const Login = () => {
   return (
     <>
       <Navbar />
-      <div class="flex-center">
-        <div class="login-card">
-          <h1 class="text-login">Login</h1>
-          <div class="input-icon-container input-primary">
+      <div className="flex-center">
+        <div className="login-card">
+          <h1 className="text-login">Login</h1>
+          <div className="input-icon-container input-primary">
             <AiOutlineMail className="input-icon" />
-            <input type="email" class="input" placeholder="e.g abc@gmail.com" />
+            <input
+              type="email"
+              className="input"
+              placeholder="e.g abc@gmail.com"
+            />
           </div>
-          <div class="input-icon-container input-primary">
+          <div className="input-icon-container input-primary">
             <AiOutlineLock className="input-icon" />
-            <input type="email" class="input" placeholder="e.g abc123" />
+            <input type="email" className="input" placeholder="e.g abc123" />
             <FaRegEyeSlash className="cursor" />
           </div>
-          <div class="horizontal-div">
-            <label className="cursor" for="remember-me">
+          <div className="horizontal-div">
+            <label className="cursor" htmlFor="remember-me">
               <input id="remember-me" type="checkbox" />
               Remember me
             </label>
-            <a class="forgot-link cursor">Forgot Password?</a>
+            <a className="htmlForgot-link cursor">htmlForgot Password?</a>
           </div>
-          <button class="btn is-solid">Login</button>
-          <span class="text-or">Or</span>
-          <p class="align-center">
+          <button className="btn is-solid">Login</button>
+          <span className="text-or">Or</span>
+          <p className="align-center">
             New user?
             <Link to="/signup">
-              <a class="auth-link cursor">Create an account</a>
+              <a className="auth-link cursor">Create an account</a>
             </Link>
           </p>
         </div>
--- src/pages/Authentication/Signup/Signup.jsx
+++ src/pages/Authentication/Signup/Signup.jsx
@@ -10,47 +10,52 @@ export const Signup = () => {
       {/* || Navbar starts  */}
       <Navbar />
       {/* || Navbar ends */}
-      <div class="flex-center">
-        <div class="signup-card">
-          <h1 class="text-signup">Signup</h1>
-          <div class="name-section">
+      <div className="flex-center">
+        <div className="signup-card">
+          <h1 className="text-signup">Signup</h1>
+          <div className="name-section">
             <input
               id="first-name"
-              class="input is-input-primary"
+              className="input is-input-primary"
               type="text"
               placeholder="Firstname*"
             />
             <input
               id="last-name"
-              class="input is-input-primary"
+              className="input is-input-primary"
               type="text"
               placeholder="Lastname*"
             />
           </div>
-          <div className="mg-top input-icon-container-su input-primary">
-            <input id="email" class="input" type="email" placeholder="Email*" />
+          <div classNameName="mg-top input-icon-container-su input-primary">
+            <input
+              id="email"
+              className="input"
+              type="email"
+              placeholder="Email*"
+            />
           </div>
-          <div class="mg-top input-icon-container-su input-primary">
+          <div className="mg-top input-icon-container-su input-primary">
             <input
               type="password"
-              class="input pd-sm"
+              className="input pd-sm"
               placeholder="Passowrd*"
             />
-            <FaRegEyeSlash className="pd-hztl-sm fs-lg" />
+            <FaRegEyeSlash classNameName="pd-hztl-sm fs-lg" />
           </div>
-          <div class="mg-top input-icon-container-su input-primary">
+          <div className="mg-top input-icon-container-su input-primary">
             <input
               type="password"
-              class="input pd-sm"
+              className="input pd-sm"
               placeholder="Confirm password*"
             />
-            <FaRegEyeSlash className="pd-hztl-sm fs-lg" />
+            <FaRegEyeSlash classNameName="pd-hztl-sm fs-lg" />
           </div>
-          <buttton class="btn is-solid">Sign Up</buttton>
-          <p class="align-center">
+          <buttton className="btn is-solid">Sign Up</buttton>
+          <p className="align-center">
             Already have an account?
             <Link to="/login">
-              <a class="auth-link">Login</a>
+              <a className="auth-link">Login</a>
             </Link>
           </p>
         </div>
