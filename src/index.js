e7925dbf8ea631978145d8d249b190668467a590
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-12-12 21:42:04 +0530

    Provider: wish list provider added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { DataStoreProvider } from "./contexts/DataStoreContext";
 import { FilterProvider } from "./contexts/FilterContext";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { WishListProvider } from "./contexts/WishListContext";
 
 // Call make Server
 makeServer();
@@ -17,13 +18,15 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Router>
-      <FilterProvider>
-        <DataStoreProvider>
-          <AuthContextProvider>
-            <App />
-          </AuthContextProvider>
-        </DataStoreProvider>
-      </FilterProvider>
+      <AuthContextProvider>
+        <WishListProvider>
+          <FilterProvider>
+            <DataStoreProvider>
+              <App />
+            </DataStoreProvider>
+          </FilterProvider>
+        </WishListProvider>
+      </AuthContextProvider>
     </Router>
   </React.StrictMode>,
 );
