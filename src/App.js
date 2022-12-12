10b9315b74e9eaf7b31c4c9821f20f64fd6f281d
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-12-12 16:20:41 +0530

    Docs: Data provider added at root

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { Routes } from "react-router-dom";
 import "./App.css";
 import { AppRoute } from "./components/Route/AppRoute";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <AppRoute />
-      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React, {StrictMode} from "react";
-import {createRoot} from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataStoreProvider } from "./contexts/DataStoreContext";
 
 // Call make Server
 makeServer();
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <DataStoreProvider>
+        <App />
+      </DataStoreProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
