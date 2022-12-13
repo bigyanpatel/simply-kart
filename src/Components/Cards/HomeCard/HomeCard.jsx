abdc130d5a610446b18faaa9bf2dfcf1905c4e48
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-12-13 21:22:31 +0530

    Style: HomeCard component css changed

--- src/Components/Cards/HomeCard/HomeCard.jsx
+++ src/Components/Cards/HomeCard/HomeCard.jsx
@@ -4,16 +4,16 @@ import "./HomeCard.css";
 export const HomeCard = ({ product }) => {
   const { title, imgSrc, sellPrice, costPrice, discount } = product;
   return (
-    <div className="card">
+    <div className="card flex-center pd-vrtl-sm">
       <div className="card-image-container">
-        <img className="book-image" src={imgSrc} alt="No preview available" />
+      <img className="book-image" src={imgSrc} alt="book cover photo" />
       </div>
       <div className="card-body">
-        <h3 className="card-title">{title}</h3>
+      <h3 className="card-title mg-top">{title}</h3>
         <p className="card-sell-price">
-          <span>{sellPrice}</span>
-          <span className="card-cost-price">{costPrice}</span>
-          <span className="card-discount">{discount}off</span>
+        <span>{sellPrice}₹</span>
+          <span className="card-cost-price">{costPrice}₹</span>
+          <span className="card-discount">{discount}%off</span>
         </p>
       </div>
     </div>
