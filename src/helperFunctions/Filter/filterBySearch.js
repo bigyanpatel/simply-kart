f57a3c2be162be8b0cef36139a75d3ddbfc3ee1c
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-12-14 21:29:15 +0530

    Fix: empty space bug fixed

--- src/helperFunctions/Filter/filterBySearch.js
+++ src/helperFunctions/Filter/filterBySearch.js
@@ -1,5 +1,5 @@
 export const getFilterBySearch = (data, searchText) => {
     return data.filter((item) =>
-      item.title.toLowerCase().includes(searchText.toLowerCase())
+    item.title.toLowerCase().includes(searchText.trim().toLowerCase())
     );
   };
